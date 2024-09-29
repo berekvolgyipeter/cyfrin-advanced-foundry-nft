f75da6a0d0ba508f92946ca86e9fa47cb8fec7df
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-09-29 15:53:20 +0200

    implement basic unit tests

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -2,9 +2,39 @@
 pragma solidity ^0.8.27;
 
 import {Test} from "forge-std/src/Test.sol";
+import {DeployBasicNft} from "script/DeployBasicNft.s.sol";
+import {BasicNft} from "src/BasicNft.sol";
 
 contract TestBasicNft is Test {
-    function setUp() external {}
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
-    function testSkeleton() public view {}
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public USER;
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+        USER = makeAddr("USER");
+    }
+
+    function assertStringEq(string memory expected, string memory actual) public pure {
+        assertEq(keccak256(abi.encodePacked(expected)), keccak256(abi.encodePacked(actual)));
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Doggie";
+        string memory actualName = basicNft.name();
+
+        assertStringEq(expectedName, actualName);
+    }
+
+    function testUserCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG_URI);
+
+        assertEq(basicNft.balanceOf(USER), 1);
+        assertStringEq(PUG_URI, basicNft.tokenURI(0));
+    }
 }
