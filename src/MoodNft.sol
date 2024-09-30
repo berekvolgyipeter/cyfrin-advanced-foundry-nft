e92dc2ace3bb526bc33540c2a77c35c0fb7ae56c
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-09-30 20:09:44 +0200

    implement mood nft base

---
+++ src/MoodNft.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {ERC721} from "openzeppelin/token/ERC721/ERC721.sol";
+import {Base64} from "openzeppelin/utils/Base64.sol";
+
+contract MoodNft is ERC721 {
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    uint256 private s_tokenCounter;
+    string private s_sadSvgUri;
+    string private s_happySvgUri;
+
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(string memory sadSvgUri, string memory happySvgUri) ERC721("Mood NFT", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvgUri = sadSvgUri;
+        s_happySvgUri = happySvgUri;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
+        s_tokenCounter++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        string memory imageURI;
+
+        if (s_tokenIdToMood[tokenId] == Mood.SAD) {
+            imageURI = s_sadSvgUri;
+        } else {
+            imageURI = s_happySvgUri;
+        }
+
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    abi.encodePacked(
+                        '{"name":"',
+                        name(),
+                        '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                        '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                        imageURI,
+                        '"}'
+                    )
+                )
+            )
+        );
+    }
+}
